b7a9514e19599f58669507b2d9b5218fa0a1aa56
willtsangaa <wltsangaa@connect.ust.hk>
2021-12-15 22:30:46 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,116 @@
+import numpy as np 
+import pandas as pd
+import requests 
+import xlsxwriter
+import math
+from secrets import IEX_CLOUD_API_TOKEN
+stocks = pd.read_csv('sp_500_stocks.csv')
+
+symbol = 'AAPL'
+api_url = f'https://sandbox.iexapis.com/stable/stock/{symbol}/quote/?token={IEX_CLOUD_API_TOKEN}'
+data = requests.get(api_url).json()
+price = data['latestPrice']
+market_cap = data['marketCap']
+my_columns = ['Ticker', 'Stock Price', 'Market Capitalization', 'Number of Share to Buy']
+
+
+
+final_dataframe = pd.DataFrame(columns = my_columns)
+for stock in stocks['Ticker'][:5]:
+    api_url = f'https://sandbox.iexapis.com/stable/stock/{stock}/quote/?token={IEX_CLOUD_API_TOKEN}'
+    data = requests.get(api_url).json()
+    final_dataframe=final_dataframe.append(
+        pd.Series(
+        [
+            stock,
+            data['latestPrice'], 
+            data['marketCap'],
+            'N/A'
+        ],
+        index = my_columns   
+        ),
+        ignore_index=True
+    )
+
+def chunks(list,n):
+    for i in range(0, len(list),n):
+        yield list[i:i+n]
+symbol_groups = list(chunks(stocks['Ticker'],100))
+symbol_strings = []
+for i in range(0, len(symbol_groups)):
+    symbol_strings.append(','.join(symbol_groups[i]))
+    #print(symbol_strings[i])
+final_dataframe = pd.DataFrame(columns = my_columns)
+
+for symbol_string in symbol_strings:
+    batchc_api_call_url = f'https://sandbox.iexapis.com/stable/stock/market/batch/?types=quote&symbols={symbol_string}&token={IEX_CLOUD_API_TOKEN}'
+    data = requests.get(batchc_api_call_url).json()
+    for symbol in symbol_string.split(','):
+        final_dataframe=final_dataframe.append(
+        pd.Series(
+        [
+            symbol,
+            data[symbol]['quote']['latestPrice'], 
+            data[symbol]['quote']['marketCap'],
+            'N/A'
+        ],
+        index = my_columns   
+        ),
+        ignore_index=True
+    )
+#allocate capital
+portfolio_size = input('Enter the value of your portfolio')
+try:
+    val = float(portfolio_size)
+    print(val)
+except ValueError:
+    print("That is not a number! \nPlease try again")
+    portfolio_size = input('Enter the value of your portfolio')
+    
+
+position_size = float(portfolio_size)/len(final_dataframe.index)
+for i in range(0, len(final_dataframe.index)):
+    final_dataframe.loc[i, 'Number Of Shares to Buy'] = math.floor(position_size / final_dataframe['Stock Price'][i])
+
+writer = pd.ExcelWriter('recommended trades.xlsx', engine = 'xlsxwriter')
+final_dataframe.to_excel(writer, 'Recommended Trades', index = False)
+background_color = '#0a0a23'
+font_color = '#ffffff'
+
+string_format = writer.book.add_format(
+    {
+        'font_color': font_color,
+        'bg_color': background_color,
+        'border': 1
+    }
+)
+
+dollar_format = writer.book.add_format(
+    {
+        'num_format': '$0.00',
+        'font_color': font_color,
+        'bg_color': background_color,
+        'border': 1
+    }
+)
+
+integer_format = writer.book.add_format(
+    {
+        'num_format': '0',
+        'font_color': font_color,
+        'bg_color': background_color,
+        'border': 1
+    }
+)
+
+column_formats = { 
+                    'A': ['Ticker', string_format],
+                    'B': ['Price', dollar_format],
+                    'C': ['Market Capitalization', dollar_format],
+                    'D': ['Number Of Shares to Buy', integer_format]
+                    }
+
+for column in column_formats.keys():
+    writer.sheets['Recommended Trades'].set_column(f'{column}:{column}', 20, column_formats[column][1])
+    writer.sheets['Recommended Trades'].write(f'{column}1', column_formats[column][0], string_format)
+writer.save()
